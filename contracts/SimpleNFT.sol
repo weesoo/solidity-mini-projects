4443ef7274972a40b9dd4e4fdfe8c0da50a69f5b
weesoo <142925784+weesoo@users.noreply.github.com>
2025-06-18 01:42:28 +0300

    add SimpleNFT ERC721 contract using OpenZeppelin

---
+++ contracts/SimpleNFT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SimpleNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint(address recipient) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+
+        return newItemId;
+    }
+}
